7c8456a479ece7a94874a945a650d1c196c05334
Keenan Alves <keenan.alves@gmail.com>
2019-09-11 18:43:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
-def load_library(file_path) 
-file_path = YAML.load_file('emoticons.yml')
+def load_library() 
+YAML.load_file(Rails.root.join('lib', 'emoticons.yml'))
 end
 
 def get_japanese_emoticon
